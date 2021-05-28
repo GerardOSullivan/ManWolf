e74ac9a2987bde784ae042b19aa0827b05b87fc0
Gerard <Gerardosullivan.work@email.com>
2021-05-28 16:26:55 +0100

    Signed-off-by: Gerard <Gerardosullivan.work@email.com>

---
+++ Man_wolf/Assets/Scripts/Player Scripts/AnimationController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private CharacterController character;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animation for running
+        if(PlayerSprintAndCrouch.is_running)
+        {
+            animator.SetBool("IsRunning",true);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+        //animation for jumping
+        if(PlayerMovement.isJumping)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+           animator.SetBool("IsJumping", false);
+        }
+
+        //animation for walking 
+        if(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
+        //animation for crouching
+        if (PlayerSprintAndCrouch.is_Crouching)
+        {
+            animator.SetBool("IsCrouching", true);
+        }
+        else
+        {
+            animator.SetBool("IsCrouching", false);
+        }
+
+
+    }
+}
---
+++ Man_wolf/Assets/Scripts/Player Scripts/SwapCameras.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCameras : MonoBehaviour
+{
+    private bool isInFirstPerson = true;
+    public GameObject firstPersonCamera;
+    public GameObject thirdPersonCamera;
+    public GameObject thirdPersonModel;
+    private void Start()
+    {
+        thirdPersonCamera.SetActive(false);
+        thirdPersonModel.SetActive(false);
+        firstPersonCamera.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //firstperson
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(!isInFirstPerson)
+            {
+                isInFirstPerson = true;
+                thirdPersonCamera.SetActive(false);
+                firstPersonCamera.SetActive(true);
+                thirdPersonModel.SetActive(false);
+
+            }       
+             //Third Person
+             else
+             {
+                isInFirstPerson = false;
+                firstPersonCamera.SetActive(false);
+                thirdPersonModel.SetActive(true);
+                thirdPersonCamera.SetActive(true);
+            }
+        }
+    }
+}
